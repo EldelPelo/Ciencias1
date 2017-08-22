03f36d84ca158a19ef5a837eef9d0d85fa4ee021
EldelPelo <cristian94var@gmail.com>
2017-08-22 10:55:00 -0500

    Update Bucket_Sort.cpp

--- Bucket_Sort.cpp
+++ Bucket_Sort.cpp
@@ -1,46 +1,38 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
- 
-void b_sort(int sarray[], int array_size) {
-    const int max = array_size;
-    // use bucket[x][max] to hold the current count
-    int bucket[10][max+1];
-    // init bucket counters
-    for(int x=0;x<10;x++)
-		bucket[x][max] = 0;
-    // main loop for each digit position
-    for(int digit = 1; digit <= 100; digit += 10) {
-        // array to bucket
-        for(int i = 0; i < max; i++) {
-            // get the digit 0-9
-            int dig = (sarray[i] / digit) % 10;
-            // add to bucket and increment count
-            bucket[dig][bucket[dig][max]++] = sarray[i];
-        }
-        // bucket to array
-        int idx = 0;
-        for(int x = 0; x < 10; x++) {
-            for(int y = 0; y < bucket[x][max]; y++) {
-                sarray[idx++] = bucket[x][y];
-            }
-            // reset the internal bucket counters
-            bucket[x][max] = 0;
-        }
+
+void ordenamiento_cubetas(int arreglo[], int tamanio){
+    int tam;
+    vector<int> cubetas[10];
+    vector<int> posicion;
+    int elemento,cubeta;
+    //LLenando cubetas
+    for(int i=0;i<tamanio;i++){
+        elemento = arreglo[i];
+        cubeta = elemento/10;
+        cubetas[cubeta].push_back(elemento);
     }
+
+    cout<<"Cubetas llenas"<<endl;
+    //Ordenando cubetas
+    for(int i=0;i<10;i++){
+        sort(cubetas[i].begin(),cubetas[i].end());
+    }
+    cout<<"Cubetas ordenadas"<<endl;
+    //Mostrar ordenado
+    for(int i=0;i<10;i++){
+        for(int j=0; j<=cubetas[i].size()-1;j++){
+            elemento = cubetas[i][j];
+            cout<<elemento<<" ";
+        }
+        cout<<" \n";
+    }
+    cout<<"Mostrando cubetas"<<endl;
 }
- 
 int main() {
-    int i, a[] = {29,25,3,49,9,37,21,43}, n = 8;
-    int SIZE=10;
-    cout<<"Before sorting:\n";
-    for(i = 0; i < n; ++i)
-        cout << a[i] << " ";
-    
-    b_sort(a,n);
-    
-    cout<< "\n\nAfter sorting:\n";
-    for(i = 0; i < n; ++i)
-        cout<< a[i] << " ";
-    
-    return 0;
+    int i, a[] = {29,25,3,49,9,37,21,43,99,67,45,81,10}, n = 13;
+    ordenamiento_cubetas(a,n);
+    return 1;
 }
