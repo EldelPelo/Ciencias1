9b595243532a663ed2a21935ce44f9fc1c514b54
EldelPelo <cristian94var@gmail.com>
2017-08-25 00:43:55 -0500

    Update Bucket_Sort.cpp

--- Bucket_Sort.cpp
+++ Bucket_Sort.cpp
@@ -1,38 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <math.h>
 using namespace std;
-
-void ordenamiento_cubetas(int arreglo[], int tamanio){
-    int tam;
-    vector<int> cubetas[10];
-    vector<int> posicion;
-    int elemento,cubeta;
-    //LLenando cubetas
-    for(int i=0;i<tamanio;i++){
-        elemento = arreglo[i];
-        cubeta = elemento/10;
-        cubetas[cubeta].push_back(elemento);
-    }
-
-    cout<<"Cubetas llenas"<<endl;
-    //Ordenando cubetas
-    for(int i=0;i<10;i++){
-        sort(cubetas[i].begin(),cubetas[i].end());
-    }
-    cout<<"Cubetas ordenadas"<<endl;
-    //Mostrar ordenado
-    for(int i=0;i<10;i++){
-        for(int j=0; j<=cubetas[i].size()-1;j++){
-            elemento = cubetas[i][j];
-            cout<<elemento<<" ";
+void ordenamiento_cubetas(int arreglo[], int tamArreglo, int mayor){
+    vector<int> cubetas[10], temporal;
+    int elemento;
+    int potencia = log10(mayor)+1;
+    for(int i=1; i<=pow(10,potencia); i*=10){
+        for(int j=0; j<tamArreglo; j++){
+            elemento = arreglo[j];
+            cubetas[(elemento/i)%10].push_back(elemento);
         }
-        cout<<" \n";
+        for(int i=0;i<10;i++){
+            for(int j=0; j<cubetas[i].size();j++){
+                elemento = cubetas[i][j];
+                temporal.push_back(elemento);
+            }
+            cubetas[i].clear();
+        }
+        for(int i=0; i<tamArreglo; i++){
+            arreglo[i]=temporal[i];
+        }
+        temporal.clear();
     }
-    cout<<"Mostrando cubetas"<<endl;
 }
-int main() {
-    int i, a[] = {29,25,3,49,9,37,21,43,99,67,45,81,10}, n = 13;
-    ordenamiento_cubetas(a,n);
-    return 1;
+int hallarMayor(int a[], int tamanio){
+    int mayor = a[0];
+    for(int i=0;i<tamanio;i++){
+        if(mayor<a[i]){
+            mayor = a[i];
+        }
+    }
+    return mayor;
+}
+int main(){
+    int a[] = {29,25,3,100,9,37,21,43,99,67,45,81,10}, n = 13, mayor;
+    mayor= hallarMayor(a,n);
+    ordenamiento_cubetas(a,n,mayor);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
 }
